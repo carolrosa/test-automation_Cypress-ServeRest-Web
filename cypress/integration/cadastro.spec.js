180ccd59828c1716e6c310be1ac9f3a396b55b77
CarolRosa <85807711+carolrosa@users.noreply.github.com>
2021-09-04 17:51:52 -0300

    Update cadastro.spec.js

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -15,14 +15,3 @@ describe("Cadastro", () => {
         cy.get('.alert').should('have.text', "nome não pode ficar em brancoemail não pode ficar em brancopassword não pode ficar em branco");
     });
 });
-
-        /*cy.intercept({
-            method: 'POST',
-            path: '/cadastrarusuario',
-        }).as(postCadastro);
-        
-        cy.wait('@postCadastro').then(({request, response}) => {
-            expect(response.statusCode).to.eq(201);
-            expect(response.body).has.property('id');
-            expect(response.body.id).is.not.null;
-        });*/
